8fa96e1a7075f8abf0d75736ffebe4d0372bc364
Jan Kurella <jan.kurella@meteogroup.com>
2021-01-25 11:56:35 +0100

    add test for sanity

--- src/test/java/privat/kurellajunior/rover/RoverSimulatorTest.java
+++ src/test/java/privat/kurellajunior/rover/RoverSimulatorTest.java
@@ -7,6 +7,8 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  *
  */
@@ -17,6 +19,9 @@ class RoverSimulatorTest {
     String testInput ="";
     StringWriter resultCollector = new StringWriter();
     RoverSimulator.runSimulation(new ByteArrayInputStream(testInput.getBytes(StandardCharsets.UTF_8)), new PrintWriter(resultCollector));
+    final String result = resultCollector.toString();
 
+    assertTrue(result.startsWith("---- begin simulation\n"));
+    assertTrue(result.endsWith("---- end simulation\n"));
   }
 }
